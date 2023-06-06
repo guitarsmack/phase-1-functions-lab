5d50b9bae5bd4639a3483ebceed46fdb5c37e78b
Michael Alan Stowe <mastowe94@gmail.com>
2023-06-06 16:58:59 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return someValue > 42 ? someValue - 42 : 42 - someValue 
+  }
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (start < destination ? destination - start : start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400) {
+        return 0;
+    } else if (distance > 400 && distance < 2000) {
+        return (distance - 400) * .02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+}
